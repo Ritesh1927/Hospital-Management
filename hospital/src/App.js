770795aeaf22462b241fcbf161ad6ad2471571e1
Aditya Singh <adityasinghedneed@gmail.com>
2024-09-18 18:54:08 +0530

    project files setup

--- hospital/src/App.js
+++ hospital/src/App.js
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Index from "./Component/Index";
 function App() {
   return (
     <div className="App">
-      <h1>this is the starting of hospital management project</h1>
-      <h2>yes bro are you ready for it </h2>
+      <Index />
     </div>
   );
 }
