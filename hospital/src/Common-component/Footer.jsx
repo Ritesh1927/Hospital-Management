583fd06e102b9301529a28a1828d72eec618533b
Aditya Singh <adityasinghedneed@gmail.com>
2024-09-19 11:51:29 +0530

    add react router dom

--- hospital/src/Common-component/Footer.jsx
+++ hospital/src/Common-component/Footer.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import "./Footer.css"
-import{Link ,Routes,Route} from "react-router-dom"
-import logo from "../assets/Icons/hospital-logo.webp"
+import React from "react";
+import "./Footer.css";
+import { Link, Routes, Route } from "react-router-dom";
+import logo from "../assets/Icons/hospital-logo.webp";
 const Footer = () => {
   return (
     <>
@@ -15,31 +15,32 @@ const Footer = () => {
         </Routes>
       </div>
 
-      <div className='container footer-main-container'>
-         <div className='inline-around  footer-child-container pt50'>
-            <div className='footer-div-1 w-20'>
-              <img className='footer-logo' src={logo} alt={"no data"} />
-              <p className='f-18 f-w500 font-fmly text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates officiis, voluptate laudantium dicta,dolorum adipisci totam aspernatur? At iusto rerum hic vero labore.</p>
-               
-            </div>
-            <div className='footer-div-2 w-25'>
-                <h1>Departments</h1>
+      <div className="container footer-main-container">
+        <div className="inline-around  footer-child-container pt50">
+          <div className="footer-div-1 w-20">
+            <img className="footer-logo" src={logo} alt={"no data"} />
+            <p className="f-18 f-w500 font-fmly text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptates officiis, voluptate laudantium dicta,dolorum adipisci
+              totam aspernatur? At iusto rerum hic vero labore.
+            </p>
+          </div>
+          <div className="footer-div-2 w-25">
+            <h1>Departments</h1>
             <ul className="department-link-element">
               <li>
-                
                 <Link to="/ Footer" className="footer-department-page-link">
                   Medicine
                 </Link>
               </li>
               <li>
                 <Link to="/ Footer" className="footer-department-page-link">
-                   Neurology
+                  Neurology
                 </Link>
               </li>
               <li>
                 <Link to="/ Footer" className="footer-department-page-link">
-                 Eye Care
-
+                  Eye Care
                 </Link>
               </li>
               <li>
@@ -49,21 +50,21 @@ const Footer = () => {
               </li>
               <li>
                 <Link to="/ Footer" className="footer-department-page-link">
-                Dental Care
+                  Dental Care
                 </Link>
               </li>
             </ul>
-            </div>
-            <div className='footer-div-3 w-25'>
-                <h1>Opening Hours</h1>
-            </div>
-            <div className='footer-div-4 w-25'>
-                <h1>Get In Touch</h1>
-            </div>
-         </div>
+          </div>
+          <div className="footer-div-3 w-25">
+            <h1>Opening Hours</h1>
+          </div>
+          <div className="footer-div-4 w-25">
+            <h1>Get In Touch</h1>
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
