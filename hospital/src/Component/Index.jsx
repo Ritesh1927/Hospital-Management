017bb2b80d59125c8b80d99f011339799cb951a8
Codejoker01 <lavjeet01@gmail.com>
2024-09-21 17:00:43 +0530

    footer and appoinment form complete

--- hospital/src/Component/Index.jsx
+++ hospital/src/Component/Index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../Common-component/Navbar";
 import Footer from "../Common-component/Footer";
+import AppoinmentPage from "./AppoinmentPage";
 function Index() {
   return (
     <div>
       <Navbar />
-
+<AppoinmentPage/>
       <Footer />
     </div>
   );
